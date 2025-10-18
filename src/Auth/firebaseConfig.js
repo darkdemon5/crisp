e4b1095fc12ed500bf2a1acadd5cd7f67c48d0d0
darkdemon5 <yash.sag55@gmail.com>
2025-10-18 22:56:21 +0530

    change asset folder, and add enviroment variables

--- src/Auth/firebaseConfig.js
+++ src/Auth/firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, setPersistence } from "firebase/auth";
+import { browserLocalPersistence, getAuth, setPersistence } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
@@ -16,5 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
-// setPersistence(auth, )
-export { db, auth };
+setPersistence(auth, browserLocalPersistence).catch((error) => {
+  console.error("Error setting persistence:", error);
+});
+export {app, db, auth };
